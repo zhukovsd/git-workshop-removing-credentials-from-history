f1c3f6d3aab0566a82854d23ae2e4f964047eae4
Sergey Zhukov <zhukovsd@gmail.com>
2026-06-13 20:57:39 +0400

    refactor(db): add cascade delete for user relations

--- src/main/resources/db/changelog/changes/002-create-locations-table.sql
+++ src/main/resources/db/changelog/changes/002-create-locations-table.sql
@@ -5,5 +5,5 @@ CREATE TABLE locations (
     latitude DECIMAL(8, 6) NOT NULL,
     longitude DECIMAL(9, 6) NOT NULL,
 
-    CONSTRAINT fk_locations_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT fk_locations_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
--- src/main/resources/db/changelog/changes/003-create-sessions-table.sql
+++ src/main/resources/db/changelog/changes/003-create-sessions-table.sql
@@ -3,5 +3,5 @@ CREATE TABLE sessions (
     user_id bigint NOT NULL,
     expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
 
-    CONSTRAINT fk_sessions_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+    CONSTRAINT fk_sessions_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
